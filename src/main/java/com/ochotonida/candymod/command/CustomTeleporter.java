aae6e9345016449eec591d4aabe3844172d11b71
Wouter <wouter@grijsloke.be>
2018-09-04 22:18:28 +0200

    Rewrote teleporter

--- src/main/java/com/ochotonida/candymod/command/CustomTeleporter.java
+++ src/main/java/com/ochotonida/candymod/command/CustomTeleporter.java
@@ -1,92 +1,45 @@
 package com.ochotonida.candymod.command;
 
 import com.ochotonida.candymod.ModConfig;
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.Teleporter;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.ITeleporter;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+public class CustomTeleporter implements ITeleporter {
 
-// see https://wiki.mcjty.eu/modding/index.php?title=Main_Page
-public class CustomTeleporter extends Teleporter {
-
-    public CustomTeleporter(WorldServer world, BlockPos pos) {
-        super(world);
-        this.worldServer = world;
-        this.x = pos.getX();
-        this.y = pos.getY();
-        this.z = pos.getZ();
+    public CustomTeleporter() {
     }
-
-    private final WorldServer worldServer;
-    private double x;
-    private double y;
-    private double z;
 
     @Override
-    public void placeInPortal(@Nonnull Entity entity, float rotationYaw) {
-        // The main purpose of this function is to *not* create a nether portal
-        this.worldServer.getBlockState(new BlockPos((int) this.x, (int) this.y, (int) this.z));
-
-        entity.setPosition(this.x, this.y, this.z);
-        entity.motionX = 0.0f;
-        entity.motionY = 0.0f;
-        entity.motionZ = 0.0f;
+    public void placeEntity(World world, Entity entity, float yaw) {
+        if (!(entity instanceof EntityPlayer)) {
+            throw new IllegalArgumentException("This teleporter can only teleport players");
+        }
+        entity.moveToBlockPosAndAngles(findTargetPos((EntityPlayer) entity, world.provider.getDimension()), yaw, entity.rotationPitch);
     }
 
-    public static boolean teleport(EntityPlayer player) {
-        int oldDimension = player.getEntityWorld().provider.getDimension();
-        EntityPlayerMP entityPlayerMP = (EntityPlayerMP) player;
-        MinecraftServer server = player.getEntityWorld().getMinecraftServer();
-        assert server != null;
-
-        if (oldDimension == ModConfig.dimensionId) {
-            WorldServer worldServer = server.getWorld(0);
-            BlockPos spawn = null;
-            BlockPos bed = entityPlayerMP.getBedLocation(0);
-            if (bed != null) {
-                spawn = EntityPlayer.getBedSpawnLocation(worldServer, bed, false);
-            }
-            if (spawn == null) {
-                spawn = worldServer.getSpawnPoint();
-            }
-            server.getPlayerList().transferPlayerToDimension(entityPlayerMP, 0, new CustomTeleporter(worldServer, spawn));
-            player.setPositionAndUpdate(spawn.getX(), spawn.getY(), spawn.getZ());
-            return true;
-        } else {
-            if (oldDimension != 1) {
-                WorldServer worldServer = server.getWorld(ModConfig.dimensionId);
-                BlockPos pos = findSpawn(player.getPosition().getX(), player.getPosition().getZ(), worldServer);
-                if (pos != null) {
-                    server.getPlayerList().transferPlayerToDimension(entityPlayerMP, ModConfig.dimensionId, new CustomTeleporter(worldServer, pos));
-                    player.setPositionAndUpdate(pos.getX(), pos.getY(), pos.getZ());
-                    return true;
-                }
+    private static BlockPos findTargetPos(EntityPlayer player, int targetWorldId) {
+        // noinspection ConstantConditions
+        World targetWorld = player.getServer().getWorld(targetWorldId);
+        BlockPos targetPos = player.getBedLocation(targetWorldId);
+        // noinspection ConstantConditions
+        if (targetPos != null) {
+            targetPos = EntityPlayer.getBedSpawnLocation(targetWorld, targetPos, false);
+            if (targetPos == null) {
+                player.sendStatusMessage(new TextComponentString("Your home bed was missing or obstructed"), false);
+                player.setSpawnChunk(null, false, targetWorldId);
             }
         }
-        return false;
-    }
-
-    @Nullable
-    private static BlockPos findSpawn(int posX, int posZ, World world) {
-        for (int x = posX; x < posX + 16; x++) {
-            for (int z = posZ; z < posZ + 16; z++) {
-                BlockPos pos = new BlockPos(x, 254, z);
-                for (int y = 254; y > 55; y--) {
-                    if (world.isAirBlock(pos) && world.isAirBlock(pos.up()) && (world.isBlockFullCube(pos.down()) || world.getBlockState(pos.down()).getMaterial() == Material.WATER)) {
-                        return pos;
-                    }
-                    pos = pos.down();
-                }
+        if (targetPos == null) { // player has no bed, or it is missing/obstructed
+            if (targetWorldId == ModConfig.dimensionId) {
+                targetPos = targetWorld.getTopSolidOrLiquidBlock(new BlockPos(player));
+            } else {
+                targetPos = targetWorld.getTopSolidOrLiquidBlock(targetWorld.getSpawnPoint());
             }
         }
-        return null;
+        return targetPos;
     }
 }
--- src/main/java/com/ochotonida/candymod/item/ItemTeleporter.java
+++ src/main/java/com/ochotonida/candymod/item/ItemTeleporter.java
@@ -1,10 +1,13 @@
 package com.ochotonida.candymod.item;
 
+import com.ochotonida.candymod.ModConfig;
 import com.ochotonida.candymod.command.CustomTeleporter;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 public class ItemTeleporter extends ModFoodItem {
@@ -13,12 +16,17 @@ public class ItemTeleporter extends ModFoodItem {
         super("teleporter", 1, 1.0F);
     }
 
+    @Nonnull
     @Override
     @ParametersAreNonnullByDefault
-    protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
-        super.onFoodEaten(stack, worldIn, player);
-        if (!worldIn.isRemote) {
-            CustomTeleporter.teleport(player);
+    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entity) {
+        if (!world.isRemote && entity instanceof EntityPlayer) {
+            if (world.provider.getDimension() == ModConfig.dimensionId) {
+                entity.changeDimension(0, new CustomTeleporter());
+            } else {
+                entity.changeDimension(ModConfig.dimensionId, new CustomTeleporter());
+            }
         }
+        return super.onItemUseFinish(stack, world, entity);
     }
 }
