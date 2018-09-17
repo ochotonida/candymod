df54a70badde4a834012a5f1989b89991e164cd6
Wouter <wouter@grijsloke.be>
2018-09-17 22:10:12 +0200

    Fixed beds exploding when trying to sleep in dimension

--- src/main/java/com/ochotonida/candymod/world/dimension/WorldProviderCandyWorld.java
+++ src/main/java/com/ochotonida/candymod/world/dimension/WorldProviderCandyWorld.java
@@ -1,10 +1,12 @@
 package com.ochotonida.candymod.world.dimension;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class WorldProviderCandyWorld extends WorldProvider {
 
@@ -34,5 +36,12 @@ public class WorldProviderCandyWorld extends WorldProvider {
     @Override
     public IChunkGenerator createChunkGenerator() {
         return new ChunkGeneratorCandyWorld(world);
+    }
+
+    @Nonnull
+    @Override
+    @ParametersAreNonnullByDefault
+    public WorldSleepResult canSleepAt(net.minecraft.entity.player.EntityPlayer player, BlockPos pos) {
+        return WorldSleepResult.ALLOW;
     }
 }
